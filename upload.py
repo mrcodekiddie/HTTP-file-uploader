dc2c423e155ffdaa18adf9890d5e273ac8de564a
mrcodekiddie <vmuthu@tuta.io>
2020-06-09 00:48:27 +0530

    added support for Windows too 💩

--- upload.py
+++ upload.py
@@ -8,18 +8,23 @@ from pathlib import Path
 try:
     file_path=Path(argument[1])
     if(file_path.is_file()):
-        print(file_path)
+        #print(file_path)
         img_upload_url = "https://e-maligai.com/api/vsm_chrompet/imageUpload.php"
         with open(str(file_path), "rb") as image_file:
-            
-            file_name=str(file_path)[::-1].split("/")[0][::-1].split(".")[0]
-            file_extension=str(file_path)[::-1].split("/")[0][::-1].split(".")[1]
 
-            print(file_name+file_extension)
+            if(os.name=="posix"):
+                file_name=str(file_path)[::-1].split(os.sep)[0][::-1].split(".")[0]
+                file_extension=str(file_path)[::-1].split(os.sep)[0][::-1].split(".")[1]
+            if(os.name=="nt"):
+                file_name=str(file_path)[::-1].split(os.sep)[0][::-1].split(".")[0]
+                file_extension=str(file_path)[::-1].split(os.sep)[0][::-1].split(".")[1]
+        
+
+            #print(file_name+file_extension)
             encoded_string = base64.b64encode(image_file.read())
             post_req= requests.post(img_upload_url,data=
             {
-                'icode':file_name,
+                'icode': file_name,
                 'extension': file_extension,
                 'image': encoded_string
             }
